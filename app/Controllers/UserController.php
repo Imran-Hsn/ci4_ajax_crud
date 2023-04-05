9f2d24ac5e3b448e54c276654b8f2e9b2b958ccb
Imran Hasan <imran.hasanpi@gmail.com>
2023-04-05 11:32:38 +0600

    add user functionality added done

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 
 class UserController extends BaseController
@@ -12,5 +13,25 @@ class UserController extends BaseController
         helper('render');
         
         return render('users/index');
+    }
+
+    public function store() {
+        $user = new UserModel();
+        $user_password = $this->request->getPost('password');
+        $hashed_password = password_hash($user_password, PASSWORD_DEFAULT);
+
+        $data = [
+            'name' => $this->request->getPost('name'),
+            'email' => $this->request->getPost('email'),
+            'phone' => $this->request->getPost('phone'),
+            'password' => $hashed_password
+        ];
+        
+        // var_dump($data); die;
+
+        $user->save($data);
+        $data = ['status'=>'User Saved Successfully'];
+
+        return $this->response->setJSON($data);
     }
 }
--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -11,22 +11,22 @@
                     <div class="form-group">
                         <div class="form-floating mb-3">
                             <input type="text" class="form-control" id="name" placeholder="Name">
-                            <label for="name">Name</label>
+                            <label for="name">Name</label> <span id="error_name" class="text-danger"></span>
                         </div>
 
                         <div class="form-floating mb-3">
                             <input type="email" class="form-control" id="email" placeholder="name@example.com">
-                            <label for="email">Email address</label>
+                            <label for="email">Email address</label> <span id="error_email" class="text-danger"></span>
                         </div>
 
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="phone" placeholder="Phone">
-                            <label for="phone">Phone</label>
+                            <input type="number" class="form-control" id="phone" placeholder="Phone">
+                            <label for="phone">Phone</label> <span id="error_phone" class="text-danger"></span>
                         </div>
 
                         <div class="form-floating">
                             <input type="password" class="form-control" id="password" placeholder="Password">
-                            <label for="password">Password</label>
+                            <label for="password">Password</label> <span id="error_password" class="text-danger"></span>
                         </div>
                     </div>
                 </div>
@@ -57,7 +57,74 @@
 <script>
     $(document).ready(function() {
         $(document).on("click", ".userData", function(){
-            alert("Hello");
+            //name validation
+            var name = $('#name').val();
+            if($.trim(name).length == 0) {
+                error_name = 'Please Enter Name';
+                $('#error_name').text(error_name);
+            }
+            else {
+                error_name = '';
+                $('#error_name').text(error_name);
+            }
+
+            //email validation
+            if($.trim($(email).val()).length == 0) {
+                error_email = 'Please Enter E-mail';
+                $('#error_email').text(error_email);
+            }
+            else {
+                error_email = '';
+                $('#error_email').text(error_email);
+            }
+
+            //phone validation
+            var phone = $('#phone').val();
+            phoneNO = $.trim(phone).length;
+            if(phoneNO == 0 || phoneNO < 11) {
+                var error_phone = 'Please Enter Valid Phone Number';
+                $('#error_phone').text(error_phone);
+            }
+            else {
+                error_phone = '';
+                $('#error_phone').text(error_phone);
+            }
+
+            //password validation
+            if($.trim($(password).val()).length == 0) {
+                error_password = 'Please Enter Password';
+                $('#error_password').text(error_password);
+            }
+            else {
+                error_password = '';
+                $('#error_password').text(error_password);
+            }
+
+            if(error_name != '' || error_email != '' || error_phone != '' || error_password != '') {
+                return false;
+            }
+            else {
+                var data = {
+                    'name' : $('#name').val(),
+                    'email' : $('#email').val(),
+                    'phone' : $('#phone').val(),
+                    'password' : $('#password').val()
+                };
+
+                var base_url = 'http://localhost/ci4_ajax_crud/public/';
+                $.ajax({
+                    type: "POST",
+                    url: base_url + "/user/store",
+                    data: data,
+                    // dataType: "dataType",
+                    success: function (response) {
+                        $('#addModal').modal('hide');
+                        $('#addModal').find('input').val();
+
+                        alert(response.status);
+                    }
+                });
+            }
         });
     }); 
     
